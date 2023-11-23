36f08d4ec4c99d123a3970de15b7ed8b670bb5ce
murdock-aubry <murdockaubry@gmail.com>
2023-11-23 12:52:16 -0500

    plot

--- read_files.py
+++ read_files.py
@@ -5,6 +5,7 @@
 
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
 
 f = h5py.File("NeutrinoML_r00725_s00000_ts868829.h5")
 
@@ -12,4 +13,12 @@ f = h5py.File("NeutrinoML_r00725_s00000_ts868829.h5")
 keys = np.array(f.keys())
 print(keys)
 
-print(f['edep_table']['energy'][()])
+print(np.size(f['edep_table']['energy'][()]))
+
+
+x = np.linspace(0, 1, np.size(f['edep_table']['energy'][()]))
+
+plt.plot(x, f['edep_table']['energy'][()])
+plt.show()
+
+
